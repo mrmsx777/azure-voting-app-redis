fb2b4b8e8e8dac7ff7b0a585ee2d97f27a440483
mrmsx777 <elattariayoub1@gmail.com>
2025-09-22 15:18:11 +0100

    rm subcription id var from main (expected fetch value from rg call)

--- infra/envs/azure-preprod/main.tf
+++ infra/envs/azure-preprod/main.tf
@@ -49,7 +49,6 @@ resource "azurerm_user_assigned_identity" "web_uami" {
   name                = "${var.app_name}-uami"
   location            = var.location
   resource_group_name = var.resource_group_name
-  subscription_id     = var.subscription_id
 }
 
 # Read ACR admin creds (enabled earlier)
